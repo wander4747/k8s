a012c5b65a0d2ab815d7d2f4350201b1ef94b0c6
wander4747 <wander.douglas14@gmail.com>
2022-05-04 23:05:46 -0300

    config map volume

--- server.go
+++ server.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/", HelloWorld)
-
+	http.HandleFunc("/configmap-volume", ConfigMapVolume)
 	http.ListenAndServe(":8000", nil)
 }
 
@@ -16,4 +18,12 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
+}
+
+func ConfigMapVolume(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("/go/src/myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+	fmt.Fprintf(w, "My Family: %s.", string(data))
 }
